d88e884ad7c73dd2b975e2ac0dbdc31f63f8acb9
Steffen Kleeberg <steffen.kleeberg@soprasteria.com>
2022-10-05 22:52:32 +0200

    add solution from dojo without obstacles and grid

--- src/main/java/marsrover/Rover.java
+++ src/main/java/marsrover/Rover.java
@@ -2,11 +2,97 @@ package marsrover;
 
 public class Rover {
 
-    public Rover() {
+    private Direction direction = Direction.N;
+    private int x;
+    private int y;
+    private final int maxX;
+    private final int maxY;
+
+    public Rover(int maxX, int maxY) {
+        this.maxX = maxX;
+        this.maxY = maxY;
     }
 
     public String execute(String command) {
-        return null;
+
+        for (char c : command.toCharArray()) {
+            if ('R' == c) {
+                turnRight();
+            } else if ('L' == c) {
+                turnLeft();
+            } else if ('M' == c) {
+                move();
+            }
+
+            normalize();
+        }
+
+        return toString();
     }
 
+    public String toString() {
+        return x + ":" + y + ":" + direction.name();
+    }
+
+    private void normalize() {
+        if (x<0) {
+            x = maxX;
+        }
+        if (y<0) {
+            y = maxY;
+        }
+        if (x>maxX){
+            x=0;
+        }
+        if (y>maxY){
+            y=0;
+        }
+    }
+
+    private void turnLeft() {
+        direction = direction.left();
+    }
+
+    private void turnRight() {
+        direction = direction.right();
+    }
+
+    private void move() {
+        if (direction == Direction.N) {
+            y++;
+        } else if (direction == Direction.E) {
+            x++;
+        } else if (direction == Direction.W) {
+            x--;
+        } else if (direction == Direction.S) {
+            y--;
+        }
+    }
+
+    enum Direction {
+        N, E, S, W;
+
+        private Direction right;
+        private Direction left;
+
+        static {
+            N.right = Direction.E;
+            E.right = Direction.S;
+            S.right = Direction.W;
+            W.right = Direction.N;
+
+            N.left = Direction.W;
+            E.left = Direction.N;
+            S.left = Direction.E;
+            W.left = Direction.S;
+        }
+
+        public Direction right() {
+            return right;
+        }
+
+        public Direction left() {
+            return left;
+        }
+    }
 }
--- src/test/java/marsrover/RoverShould.java
+++ src/test/java/marsrover/RoverShould.java
@@ -1,22 +1,56 @@
 package marsrover;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoverShould {
-    Rover rover;
+    private Rover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = new Rover(2, 3);
+    }
 
     @Test
     void start_at_position_0_0_N() {
-        rover = new Rover();
         Assertions.assertThat(rover.execute("")).isEqualTo("0:0:N");
     }
 
     @Test
     void move_to_position_2_3_N() {
-        rover = new Rover();
         Assertions.assertThat(rover.execute("MMRMMLM")).isEqualTo("2:3:N");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', 0:0:N",
+            "M, 0:1:N",
+            "RM, 1:0:E",
+            "LM, 2:0:W",
+            "RRM, 0:3:S",
+            "MMMM, 0:0:N",
+            "RMMM, 0:0:E",
+            "MMRMMLM, 2:3:N",
+    })
+    void should_move(String command, String expectedPosition) {
+        assertThat(rover.execute(command)).isEqualTo(expectedPosition);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"R, E", "RR, S", "RRR, W", "RRRR, N"})
+    void should_turn_right(String command, String expectedDirection) {
+        assertThat(rover.execute(command)).isEqualTo("0:0:"+expectedDirection);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"L, W", "LL, S", "LLL, E", "LLLL, N"})
+    void should_turn_left(String command, String expectedDirection) {
+        assertThat(rover.execute(command)).isEqualTo("0:0:"+expectedDirection);
     }
 }
